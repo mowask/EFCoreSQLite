aabd3f40ab4b1429822c1851d0bb34c80c099de6
Студент <student@Top.Academy>
2024-11-25 21:08:50 +0300

    test branch

--- 11-25 SQLite/Program.cs
+++ 11-25 SQLite/Program.cs
@@ -1,11 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
-
-
-
-
-
-
-
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
