57d29ee2049d369f4af92c9bdffecbada6d655b9
Алексей Косов <ask-87@bk.ru>
2024-11-27 03:06:27 +0300

    qwer

--- 11-25 SQLite/Program.cs
+++ 11-25 SQLite/Program.cs
@@ -1,11 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
-
-
-
-
-
-
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello world");
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello world");
+        }
 
