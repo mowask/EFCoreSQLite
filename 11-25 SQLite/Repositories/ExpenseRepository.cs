3a50d76f5279f6a70789a3571246f53f1cb7f8eb
Студент <student@Top.Academy>
2024-11-27 21:20:32 +0300

    wpf

--- 11-25 SQLite/Repositories/ExpenseRepository.cs
+++ 11-25 SQLite/Repositories/ExpenseRepository.cs
@@ -1,6 +1,9 @@
 ﻿using ExpensesTraker_Library.Contexts;
+using ExpensesTraker_Library.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +14,62 @@ namespace ExpensesTraker_Library.Repositories
     {
         private readonly ExpensesTrackerContext _context;
 
-        public ExpenseRepository ( ExpensesTrackerContext context)
+        public ExpenseRepository(ExpensesTrackerContext context)
         {
             _context = context;
         }
+
+        public ObservableCollection<Category> GetAllCategoriesObservable()
+        {
+            _context.Categories.Load();
+            return _context.Categories.Local.ToObservableCollection();
+        }
+
+        public void AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges(true);
+        }
+
+        public void RemoveCategory(int categoryId)
+        {
+            var category = _context.Categories.Find(categoryId);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges(true);
+            }
+        }
+
+        public void AddExpense(Expense expense)
+        {
+            _context.Expenses.Add(expense);
+            _context.SaveChanges(true); 
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public void RemoveExpense(int expenseId)
+        {
+            var expense = _context.Expenses.Find(expenseId);
+            if (expense != null)
+            {
+                _context.Expenses.Remove(expense);
+                _context.SaveChanges(true);
+            }
+        }       
+
+         public IEnumerable<Expense> GetAllExpenses () 
+        {
+            return _context.Expenses.ToList();
+        }
+
+        
+
+
+
     }
 }
